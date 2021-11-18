1c941e177c472fe8dbf15aceecb0e2350db5bf7f
ekkys99 <ekkys99@gmail.com>
2021-11-19 02:36:39 +0700

    Menmabhkan class active untuk page yg sedang aktif

--- resources/views/layout/main.blade.php
+++ resources/views/layout/main.blade.php
@@ -8,31 +8,10 @@
     <!-- Bootstrap CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
 
-    <title>WPU Blog | Home</title>
+    <title>WPU Blog | {{ $title }}</title>
   </head>
   <body>
-        <nav class="navbar navbar-expand-lg navbar-dark bg-danger">
-            <div class="container">
-                <a class="navbar-brand" href="#">Navbar</a>
-                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
-                </button>
-                <div class="collapse navbar-collapse" id="navbarNav">
-                <ul class="navbar-nav">
-                    <li class="nav-item">
-                    <a class="nav-link active" aria-current="page" href="/">Home</a>
-                    </li>
-                    <li class="nav-item">
-                    <a class="nav-link" href="/about">About</a>
-                    </li>
-                    <li class="nav-item">
-                    <a class="nav-link" href="/blog">Blog</a>
-                    </li>
-                </ul>
-                </div>
-            </div>
-        </nav>
-
+    @include('partial.navbar')
         <div class="container mt-4">
             @yield('container')
         </div>
--- routes/web.php
+++ routes/web.php
@@ -14,15 +14,20 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('home');
+    return view('home', [
+        "title" => "Home"
+    ]);
 });
 Route::get('/about', function () {
     return view('about', [
+        "title" => "About",
         "name" => "Wati",
         "email" => "ekkys99@gmail.com",
         "image" => "ekky.jpg"
     ]);
 });
 Route::get('/blog', function () {
-    return view('posts');
+    return view('posts', [
+        "title"=> "Posts",
+    ]);
 });
