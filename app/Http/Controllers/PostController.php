5c81fd65394bc8f169530463306e70c66315af23
ekkys <ekkys99@gmail.com>
2021-12-16 07:36:33 +0700

    Search All, in  category & auhtor fix

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -12,15 +12,20 @@ class PostController extends Controller
     public function index()
     {
         $title = '';
-        if (request('category')) {
-            $category = Category::firstWhere('slug', $request('category'));
-            $title = ' in '.$category;
+        if (request('category') || ) {
+            $category = Category::firstWhere('slug', request('category'));
+            $title = ' in '.$category->name;
         }
 
         if (request('author')) {
-            $author = User::firstWhere('username', $request('author'));
-            $title = ' by '. $author;
+            $author = User::firstWhere('username', request('author'));
+            $title = ' by '. $author->name;
         }
+
+        // if (request('categories')) {
+        //     $category = Category::firstWhere('slug', request('category'));
+        //     $title = ' in '.$category->name;
+        // }
 
         return view('posts', [
             "title" => "All Posts". $title,
