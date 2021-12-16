c44f3b939fb53945d7aa079535cce588319382e8
ekkys <ekkys99@gmail.com>
2021-12-16 08:06:04 +0700

    Add Pagonation Fix

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -30,7 +30,7 @@ class PostController extends Controller
         return view('posts', [
             "title" => "All Posts". $title,
             "active" => "posts",
-            "posts" =>  Post::latest()->filter(request(['cari', 'category', 'author']))->paginate(7)
+            "posts" =>  Post::latest()->filter(request(['cari', 'category', 'author']))->paginate(7)->withQueryString()
         ]);
     }
 
