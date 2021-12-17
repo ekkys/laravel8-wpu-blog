d4762ca992ab8dbee2ae4d3148b418a5c4ae8f9f
ekkys <ekkys99@gmail.com>
2021-12-17 15:35:45 +0700

    Add create view

--- resources/views/dashboard/posts/index.blade.php
+++ resources/views/dashboard/posts/index.blade.php
@@ -4,7 +4,8 @@
     <div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
         <h1 class="h2">My Post</h1>
     </div>
-          <div class="table-responsive col-lg-9">
+      <div class="table-responsive col-lg-12">
+          <a href="/dashboard/posts/create" class="btn btn-primary"> <span data-feather="plus"></span> Create New Post</a>
         <table class="table table-striped table-sm">
           <thead>
             <tr>
