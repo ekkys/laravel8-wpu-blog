5f1cfd67e0d50c8af5e0b0742baf53f305eeee72
ekkys99 <ekkys99@gmail.com>
2021-12-22 06:10:24 +0700

    Kirim posts ok

--- resources/views/dashboard/posts/create.blade.php
+++ resources/views/dashboard/posts/create.blade.php
@@ -15,7 +15,20 @@
             </div>
             <div class="mb-3">
             <label for="slug" class="form-label">Slug</label>
-                 <input type="text" class="form-control" id="slug" name="slug" disabled readonly>
+                 <input type="text" class="form-control" id="slug" name="slug"  readonly>
+            </div>
+            <div class="mb-3">
+            <label for="category" class="form-label">Category</label>
+                <select class="form-select" name="category_id">
+                    @foreach ($categories as $category)
+                    <option value="{{ $category->id }}">{{ $category->name }}</option>
+                    @endforeach
+                </select>
+            </div>
+            <div class="mb-3">
+            <label for="body" class="form-label">Body</label>
+                <input id="body" type="hidden" name="body">
+                <trix-editor input="body"></trix-editor>
             </div>
 
           <button type="submit" class="btn btn-primary">Create</button>
@@ -35,7 +48,12 @@
 
         })
 
+        //menonaktifkan  fungsi attachment trix
+        document.addEventListener('trix-file-accept', function(e) {
+            e.preventDefault();
+        })
 
      
     </script>
 @endsection
+
