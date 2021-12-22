08e1fb970060c1efe087e83acc5920304120c2bc
ekkys <ekkys99@gmail.com>
2021-12-22 11:03:50 +0700

    Add Delete in page

--- resources/views/dashboard/posts/show.blade.php
+++ resources/views/dashboard/posts/show.blade.php
@@ -7,9 +7,14 @@
     <div class="row my-3">
         <div class="col-md-8">
             <h2 class = "mb-3">{{ $post->title }}</h2>
-            <a href="/dashboard/posts" class="btn btn-success mb-3"> <span data-feather="arrow-left"></span>Back to All my posts</a>
-            <a href="" class="btn btn-warning mb-3"> <span data-feather="edit-3"></span>Edit</a>
-            <a href="" class="btn btn-danger mb-3"> <span data-feather="x-square"></span>Delete</a>
+            <a href="/dashboard/posts" class="btn btn-success mb-3"> <span data-feather="arrow-left"></span> Back to All my posts</a>
+            <a href="" class="btn btn-warning mb-3"> <span data-feather="edit-3"></span> Edit</a>
+
+            <form action="/dashboard/posts/{{ $post->slug  }}" method="post" class="d-inline">
+                @method('delete')
+                @csrf
+                <button class="btn btn-danger border-0 mb-3" onclick="return confirm('Are you sure?') "><span data-feather="x-circle" ></span> Delete</button>
+            </form>
             <img src="https://source.unsplash.com/1200x400?{{ $post->category->name}}" class="card-img-top" alt="{{ $post->category->name}}" class="img-fluid">
              <article class="my-3 fs6">
                  {!! $post->body !!}
