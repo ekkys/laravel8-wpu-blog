225697c1bf3bcd67ca3cd819488ebcb28e37d67d
ekkys <ekkys99@gmail.com>
2021-12-22 09:45:55 +0700

    Create post & notif fix

--- resources/views/dashboard/posts/create.blade.php
+++ resources/views/dashboard/posts/create.blade.php
@@ -11,23 +11,40 @@
             @csrf
             <div class="mb-3">
             <label for="title" class="form-label">Title</label>
-                 <input type="text" class="form-control" id="title" name="title">
+                 <input type="text" class=" form-control @error('title') is-invalid @enderror " id="title" name="title" value="{{ old('title') }}"  required autofocus>
+                 @error('title')
+                     <div class="invalid-feedback">
+                        {{ $message }}
+                     </div>
+                 @enderror
             </div>
             <div class="mb-3">
             <label for="slug" class="form-label">Slug</label>
-                 <input type="text" class="form-control" id="slug" name="slug"  readonly>
+                 <input type="text" class="form-control @error('slug') is-invalid @enderror " id="slug" name="slug" value="{{ old('slug') }}" required  readonly>
+                @error('slug')
+                    <div class="invalid-feedback">
+                    {{ $message }}
+                    </div>
+                @enderror
             </div>
             <div class="mb-3">
             <label for="category" class="form-label">Category</label>
                 <select class="form-select" name="category_id">
                     @foreach ($categories as $category)
-                    <option value="{{ $category->id }}">{{ $category->name }}</option>
+                    @if (old('category_id') == $category->id)
+                        <option value="{{ $category->id }}" selected>{{ $category->name }}</option>
+                    @else
+                        <option value="{{ $category->id }}">{{ $category->name }}</option>
+                    @endif
                     @endforeach
                 </select>
             </div>
             <div class="mb-3">
             <label for="body" class="form-label">Body</label>
-                <input id="body" type="hidden" name="body">
+                <input id="body" type="hidden" name="body" value="{{ old('body') }}"s>
+                @error('body')
+                 <p class="text-danger">{{ $message }}</p>
+                @enderror
                 <trix-editor input="body"></trix-editor>
             </div>
 
@@ -53,7 +70,7 @@
             e.preventDefault();
         })
 
-     
+
     </script>
 @endsection
 
