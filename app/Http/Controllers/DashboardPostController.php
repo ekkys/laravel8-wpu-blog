9a5c88aba2ba5a5ef358bf7fc5814f3ed907f7db
ekkys <ekkys99@gmail.com>
2021-12-24 10:33:08 +0700

    Update Image & remove the oldImage

--- app/Http/Controllers/DashboardPostController.php
+++ app/Http/Controllers/DashboardPostController.php
@@ -7,6 +7,7 @@ use App\Models\Category;
 use Illuminate\Http\Request;
 use \Cviebrock\EloquentSluggable\Services\SlugService;
 use Illuminate\Support\Str;
+use Illuminate\Support\Facades\Storage;
 
 class DashboardPostController extends Controller
 {
@@ -104,6 +105,7 @@ class DashboardPostController extends Controller
         $rules = [
             'title' => 'required|max:255',
             'category_id' => 'required',
+            'image' => 'image|file|max:1024 ',
             'body' => 'required'
         ];
 
@@ -113,11 +115,19 @@ class DashboardPostController extends Controller
 
         $validatedData = $request->validate($rules);
 
+        if($request->file('image')){
+            if($request->oldImage){
+                Storage::delete($request->oldImage);
+            }
+
+            $validatedData['image'] = $request->file('image')->store('post-images');
+        }
+
         $validatedData['user_id'] = auth()->user()->id;
         $validatedData['excerpt'] = Str::limit(strip_tags($request->body), 200);
 
         Post::where('id', $post->id)
-              ->updated($validatedData);
+              ->update($validatedData);
 
         return redirect('dashboard/posts')->with('success', 'Post has been updated!');
     }
--- resources/views/dashboard/posts/edit.blade.php
+++ resources/views/dashboard/posts/edit.blade.php
@@ -7,7 +7,7 @@
     </div>
 
     <div class="col-md-8">
-        <form action="/dashboard/posts/{{ $post->slug }}" method="post" class="mb-5">
+        <form action="/dashboard/posts/{{ $post->slug }}" method="post" class="mb-5" enctype="multipart/form-data">
             @method('put')
             @csrf
             <div class="mb-3">
@@ -41,8 +41,23 @@
                 </select>
             </div>
             <div class="mb-3">
+                <label for="image" class="form-label">Post Image</label>
+                <input type="hidden" name="oldImage" value="{{ $post->image }}">
+                @if ($post->image)
+                <img src="{{ asset('storage/'. $post->image) }}" style="display:block;" class="img-preview img-fluid mb-3 col-sm-5">
+                @else
+                <img class="img-preview img-fluid mb-3 col-sm-5">
+                @endif
+                <input class="form-control @error('image') is-invalid @enderror" type="file" id="image" name="image" onchange="previewImage()">
+                    @error('image')
+                        <div class="invalid-feedback">
+                        {{ $message }}
+                        </div>
+                    @enderror
+            </div>
+            <div class="mb-3">
             <label for="body" class="form-label">Body</label>
-                <input id="body" type="hidden" name="body" value="{{ old('body', $post->body ) }}"s>
+                <input id="body" type="hidden" name="body" value="{{ old('body', $post->body ) }}">
                 @error('body')
                  <p class="text-danger">{{ $message }}</p>
                 @enderror
@@ -71,6 +86,23 @@
             e.preventDefault();
         })
 
+        function previewImage() {
+            const image = document.querySelector('#image');
+            const imgPreview = document.querySelector('.img-preview');
+
+            console.log(image);
+            console.log(imgPreview);
+
+            imgPreview.style.display = 'block';
+
+            const oFReader = new FileReader();
+            oFReader.readAsDataURL(image.files[0]);
+
+            oFReader.onload = function (oFREvent) {
+                imgPreview.src = oFREvent.target.result;
+            }
+
+        }
 
     </script>
 @endsection
