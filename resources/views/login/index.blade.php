e8144f42a488c4a2c68258f5102fc9ebb8fed4c5
ekkys99 <ekkys99@gmail.com>
2021-12-25 20:26:31 +0700

    Add Login page

--- resources/views/login/index.blade.php
+++ resources/views/login/index.blade.php
@@ -1,47 +1,71 @@
-@extends('layout.main')
-@section('container')
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
 
-<div class="row justify-content-center">
-    <div class="col-md-4">
+    <!-- Bootstrap CSS -->
+    <link
+      href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css"
+      rel="stylesheet"
+      integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3"
+      crossorigin="anonymous"
+    />
 
-        @if(session()->has('success'))
-        <div class="alert alert-success alert-dismissible fade show" role="alert">
-            {{ session('success')}}
-            <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
-        </div>
-        @endif
+    <!-- Custom styles for this template -->
+    <link href="/css/signin.css" rel="stylesheet">
+    
+    <title>Login | Kavaliya.com</title>
+  </head>
+  <body>
+ 
+      
+      
+      <main class="form-signin">
+          
+          <h1 class="h2 mb-3 fw-normal text-center">Welcome To | Kavaliya.com</h1>
+          @if(session()->has('success'))
+          <div class="alert alert-success alert-dismissible fade show" role="alert">
+              {{ session('success')}}
+              <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+          </div>
+          @endif
+    
+          @if(session()->has('loginError'))
+          <div class="alert alert-danger alert-dismissible fade show" role="alert">
+              {{ session('loginError')}}
+              <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+          </div>
+          @endif
+        
+                <form action="/login" method="post">
+                    @csrf
+                    <div class="form-floating">
+                        <input type="email" name="email" class="form-control @error('email') is-invalid @enderror " id="email" placeholder="name@example.com" autofocus required>
+                        <label for="email">Email address</label>
+                        @error('email')
+                            <div class="invalid-feedback">
+                                {{ $message }}
+                            </div>
+                        @enderror
+                    </div>
+                    <div class="form-floating">
+                        <input type="password" name="password" class="form-control" id="password" placeholder="Password" required>
+                        <label for="password">Password</label>
+                    </div>
+        
+                <button class="w-100 btn btn-lg btn-dark" type="submit">Login</button>
+                <p class="mt-5 mb-3 text-muted">&copy; 2022 Kavaliya.com</p>
+                </form>
+            </main>
+   
 
-        @if(session()->has('loginError'))
-        <div class="alert alert-danger alert-dismissible fade show" role="alert">
-            {{ session('loginError')}}
-            <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
-        </div>
-        @endif
-
-        <main class="form-signin">
-            <form action="/login" method="post">
-                @csrf
-                <h1 class="h3 mb-3 fw-normal text-center">Please Login!</h1>
-
-                <div class="form-floating">
-                    <input type="email" name="email" class="form-control @error('email') is-invalid @enderror " id="email" placeholder="name@example.com" autofocus required>
-                    <label for="email">Email address</label>
-                    @error('email')
-                        <div class="invalid-feedback">
-                            {{ $message }}
-                        </div>
-                    @enderror
-                </div>
-                <div class="form-floating">
-                    <input type="password" name="password" class="form-control" id="password" placeholder="Password" required>
-                    <label for="password">Password</label>
-                </div>
-
-                <button class="w-100 btn btn-lg btn-primary" type="submit">Login</button>
-            </form>
-              <small class="d-block text-center mt-3">Not register? <a href="/register">Register now</a></small>
-          </form>
-        </main>
-    </div>
-</div>
-@endsection
+    <!-- Option 1: Bootstrap Bundle with Popper -->
+    <script
+      src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"
+      integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p"
+      crossorigin="anonymous"
+    ></script>
+  </body>
+</html>
